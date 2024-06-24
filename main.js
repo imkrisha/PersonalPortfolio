78d76ba9a24b4434d4d44123895ea0088fae0d25
imkrisha <krishapatel5301@gmail.com>
2024-06-24 10:38:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+function myMenuFunction() {
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsible"; // Added space before the new class name
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+/*-----------dark Mode---------------*/
+
+const body = document.querySelector("body"),
+    toggleSwitch = document.getElementById("toggle");
+
+if (toggleSwitch) {
+    toggleSwitch.addEventListener("click", ()=> { // Changed "onClick" to "click"
+    body.classList.toggle("dark");
+    });
+}
+
+/*------------Typing effect----------------*/
+
+var typingEffect = new Typed(".typedText", {
+    strings: ["Designer", "Coder", "Developer"],
+    loop: true,
+    typeSpeed: 100,
+    backDelay:2000,
+
+})
+
+/*---------scroll animation-------------*/
+
+const sr = scrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+})
+
+sr.reveal(".featured-name", {delay:100});
+sr.reveal(".text-info", {delay:200});
+sr.reveal(".text-btn", {delay:200});
+sr.reveal(".social_icons", {delay:200});
+sr.reveal(".featured-image", {delay:320});
+
+sr.reveal(".project-box", {interval:200});
+
+sr.reveal(".top-header", {});
+
+const srLeft= scrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration:2000,
+    reset: true,
+})
+
+srLeft.reveal(".about-info", { delay: 100});
+srLeft.reveal(".contact-info", { delay: 100});
+
+const srRight= scrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration:2000,
+    reset: true,
+})
+
+srRight.reveal(".skill", { delay: 100});
+srRight.reveal(".skill-box", { delay: 100});
+
+/**---------active link--------- */
+
+const sections = document.querySelectorAll(".section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+
+            sectionTop = current.offsetHeight - 50,
+            sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop +  sectionHeight) {
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.add("active-link");
+        } else {
+            document
+            .querySelector(".nav-menu a[href*=" + sectionId + "]")
+            .classList.remove("active-link");
+        }
+    });    
+
+}
+
+window.addEventListener("scroll", scrollActive);
